9d0fc17db27b85a9c72874b036920702ef046c8f
yokesh7373@gmail.com <gitHub@1>
2022-10-18 20:26:41 +0530

    Extended Binder class

--- app/src/main/java/com/training/simpleboundservice/MyBoundService.java
+++ app/src/main/java/com/training/simpleboundservice/MyBoundService.java
@@ -2,14 +2,37 @@ package com.training.simpleboundservice;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
-public class MyBoundService extends Service {
-    public MyBoundService() {
-    }
+import java.util.Random;
 
+public class MyBoundService extends Service {
+
+    // Binder given to clients
+    private final IBinder binder = new MyLocalBinder();
+
+    // Random number generator
+    private final Random mGenerator = new Random();
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        // Return this instance of LocalService so clients can call public methods
+        return binder;
+    }
+
+    public class MyLocalBinder extends Binder {
+        MyBoundService getService(){
+            return MyBoundService.this;
+        }
+    }
+
+    /** method for clients */
+    public int getRandomNumber() {
+        return mGenerator.nextInt(100);
     }
 }
